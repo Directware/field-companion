55f7eb8f28d54553fa67394fca786fc5305a2021
Mateusz Klimentowicz <m.klimentowicz@one-unity.de>
2023-01-29 15:54:32 +0100

    added settings providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,22 @@
 import 'dart:developer';
 
-import 'package:field_companion/features/app_settings/domain/app_settings.dart';
 import 'package:field_companion/features/field_service/domain/report_entry.dart';
 import 'package:field_companion/features/territory_cards/domain/drawing.dart';
 import 'package:field_companion/features/territory_cards/domain/publisher.dart';
 import 'package:field_companion/features/territory_cards/domain/territory.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() async {
   await _initDatabase();
-  runApp(const MyApp());
+
+  // For widgets to be able to read providers, we need to wrap the entire
+  // application in a "ProviderScope" widget.
+  // This is where the state of our providers will be stored.
+  runApp(const ProviderScope(
+    child: MyApp(),
+  ));
 }
 
 /// Um die Adapter zu generieren muss folgender Befehl ausgeführt werden: flutter packages pub run build_runner build
@@ -20,7 +26,6 @@ Future<void> _initDatabase() async {
   await Hive.initFlutter();
 
   Hive.registerAdapter(ReportEntryAdapter());
-  Hive.registerAdapter(AppSettingsAdapter());
   Hive.registerAdapter(PublisherAdapter());
   Hive.registerAdapter(DrawingAdapter());
   Hive.registerAdapter(TerritoryAdapter());
