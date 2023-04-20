02333e28fed87b69b9b01bc0aed9b56108aa09ec
FKanzler <fabien.kanzler@gmail.com>
2023-04-20 17:58:17 +0200

    added stacktrace

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,20 @@ import 'package:field_companion/features/core/presentation/providers/database_pr
 import 'package:field_companion/features/core/presentation/providers/shared_preferences_provider.dart';
 import 'package:field_companion/features/core/presentation/widgets/field_companion.dart';
 import 'package:field_companion/features/field_service/domain/models/report.dart';
+import 'package:field_companion/features/field_service/domain/models/studies.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:isar/isar.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:stack_trace/stack_trace.dart' as stack_trace;
 
 void main() async {
+  FlutterError.demangleStackTrace = (StackTrace stack) {
+    if (stack is stack_trace.Trace) return stack.vmTrace;
+    if (stack is stack_trace.Chain) return stack.toTrace().vmTrace;
+    return stack;
+  };
+
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
 
@@ -27,7 +35,7 @@ void main() async {
   );
 
   final sharedPreferences = await SharedPreferences.getInstance();
-  final database = await Isar.open([ReportSchema]);
+  final database = await Isar.open([ReportSchema, StudiesSchema]);
 
   runApp(
     ProviderScope(
