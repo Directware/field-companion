cb461f5b0ea1cff5d6f2c13f039bc630d095347d
FKanzler <fabien.kanzler@gmail.com>
2023-04-24 00:55:59 +0200

    fixed dummy test

--- test/features/core/presentation/widget/welcome_test.dart
+++ test/features/core/presentation/widget/welcome_test.dart
@@ -2,7 +2,9 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() async {
   group("Comparing bites", () {
-    expect(true, true);
+    test('digits', () {
+      expect(true, true);
+    });
   });
   /*
   group("Welcome widget", () {
