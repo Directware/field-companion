3e424107465fdbef6f6d3a486e744f522defffea
Tom Ludwig <tommludwig@icloud.com>
2024-03-22 14:41:49 +0100

    Fix formatting and add debug prints

--- utils/territory_converter/main.dart
+++ utils/territory_converter/main.dart
@@ -4,6 +4,7 @@ import 'dart:io';
 import 'package:archive/archive.dart';
 import 'package:field_companion/features/territories/domain/models/territory.dart';
 import 'package:field_companion/features/territories/domain/models/visit_ban.dart';
+import 'package:flutter/foundation.dart';
 import 'package:nanoid/nanoid.dart';
 import "package:path/path.dart";
 
@@ -31,14 +32,18 @@ List<String> getInputFiles() {
 }
 
 void convert(String path) {
-  print("convert: $path");
+  if (kDebugMode) {
+    print("convert: $path");
+  }
   final data = extractData(path);
   final territoryV1 = parseTerritoryV1(data);
   final territoryV2 = toTerritoryV2(territoryV1);
   final zippedData = gzipData(territoryV2);
   final outputPath = getOutputPath(path);
   writeConvertedFile(outputPath, zippedData);
-  print("-> converted: $path");
+  if (kDebugMode) {
+    print("-> converted: $path");
+  }
 }
 
 String extractData(String path) {
