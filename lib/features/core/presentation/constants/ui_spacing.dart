585c1931b624aef4fbcd3ca9a1bde1d0f5e23c4c
Mateusz Klimentowicz <mail@klimentowicz.com>
2024-06-14 10:08:20 +0200

    fix: blur & styles (#11)

---
+++ lib/features/core/presentation/constants/ui_spacing.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/widgets.dart';
+
+class UiSpacing {
+  // Border Radius Constants
+  static BorderRadius radiusRounded = BorderRadius.circular(999.0);
+  static BorderRadius radiusXl = BorderRadius.circular(20.0);
+  static BorderRadius radiusL = BorderRadius.circular(14.0);
+  static BorderRadius radiusM = BorderRadius.circular(8.0);
+  static BorderRadius radiusS = BorderRadius.circular(4.0);
+  static BorderRadius radiusNone = BorderRadius.circular(0.0);
+
+  // Spacing Constants
+  static const double appPaddingDesktop = 24.0;
+  static const double appPaddingMobile = 18.0;
+  static const double spacingXl = 80.0;
+  static const double spacingL = 48.0;
+  static const double spacingM = 24.0;
+  static const double spacingS = 16.0;
+  static const double spacingXs = 8.0;
+}
--- lib/features/core/presentation/views/welcome.dart
+++ lib/features/core/presentation/views/welcome.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:easy_localization/easy_localization.dart';
+import 'package:field_companion/features/core/presentation/constants/ui_spacing.dart';
 import 'package:field_companion/features/settings/presentation/providers/app_initialisation_provider.dart';
 import 'package:field_companion/features/settings/presentation/providers/user_language_provider.dart';
 import 'package:flutter/material.dart';
@@ -20,140 +23,195 @@ class Welcome extends ConsumerWidget {
               image: AssetImage("assets/images/background-map.jpeg"),
             ),
           ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                key: Key("welcome:title"),
-                "title",
-                style: TextStyle(color: Colors.white, fontSize: 42),
-              ).tr(),
-              const SizedBox(height: 4),
-              const Text(
-                key: Key("welcome:origin"),
-                "welcome.origin",
-                style: TextStyle(color: Colors.white, fontSize: 24),
-              ).tr(),
-              const SizedBox(height: 85),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    decoration: const BoxDecoration(
-                      color: Color.fromARGB(24, 173, 177, 184),
-                      borderRadius: BorderRadius.all(Radius.circular(500)),
-                    ),
-                    padding: const EdgeInsets.all(13),
-                    margin: const EdgeInsets.fromLTRB(0, 0, 16, 0),
-                    child: const Icon(
-                      color: Color(0xFFadb1b8),
-                      size: 24,
-                      FeatherIcons.clock,
-                    ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: UiSpacing.spacingM + UiSpacing.spacingXs,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  key: Key("welcome:title"),
+                  "title",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 38,
+                    fontWeight: FontWeight.w600,
                   ),
-                  SizedBox(
-                    width: 240,
+                ).tr(),
+                const SizedBox(height: UiSpacing.spacingXs),
+                const Text(
+                  key: Key("welcome:origin"),
+                  "welcome.origin",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ).tr(),
+                const SizedBox(height: 85),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ClipRRect(
+                      child: BackdropFilter(
+                        filter: ImageFilter.blur(sigmaX: 30, sigmaY: 30),
+                        child: Container(
+                          decoration: const BoxDecoration(
+                            color: Color.fromARGB(24, 173, 177, 184),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(500)),
+                          ),
+                          padding: const EdgeInsets.all(20),
+                          margin: const EdgeInsets.fromLTRB(0, 0, 16, 0),
+                          child: const Icon(
+                            color: Color(0xFFadb1b8),
+                            size: 20,
+                            FeatherIcons.clock,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 240,
+                      child: const Text(
+                        "welcome.feature1",
+                        style:
+                            TextStyle(color: Color(0xFFadb1b8), fontSize: 18),
+                      ).tr(),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 32),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ClipRRect(
+                      child: BackdropFilter(
+                        filter: ImageFilter.blur(sigmaX: 30, sigmaY: 30),
+                        child: Container(
+                          decoration: const BoxDecoration(
+                            color: Color.fromARGB(24, 173, 177, 184),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(500)),
+                          ),
+                          padding: const EdgeInsets.all(20),
+                          margin: const EdgeInsets.fromLTRB(0, 0, 16, 0),
+                          child: const Icon(
+                            color: Color(0xFFadb1b8),
+                            size: 20,
+                            FeatherIcons.award,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 240,
+                      child: const Text(
+                        "welcome.feature2",
+                        style:
+                            TextStyle(color: Color(0xFFadb1b8), fontSize: 18),
+                      ).tr(),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 32),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ClipRRect(
+                      child: BackdropFilter(
+                        filter: ImageFilter.blur(sigmaX: 30, sigmaY: 30),
+                        child: Container(
+                          decoration: const BoxDecoration(
+                            color: Color.fromARGB(24, 173, 177, 184),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(500)),
+                          ),
+                          padding: const EdgeInsets.all(20),
+                          margin: const EdgeInsets.fromLTRB(0, 0, 16, 0),
+                          child: const Icon(
+                            color: Color(0xFFadb1b8),
+                            size: 20,
+                            FeatherIcons.mail,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 240,
+                      child: const Text(
+                        "welcome.feature3",
+                        style:
+                            TextStyle(color: Color(0xFFadb1b8), fontSize: 18),
+                      ).tr(),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 32),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ClipRRect(
+                      child: BackdropFilter(
+                        filter: ImageFilter.blur(sigmaX: 30, sigmaY: 30),
+                        child: Container(
+                          decoration: const BoxDecoration(
+                            color: Color.fromARGB(24, 173, 177, 184),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(500)),
+                          ),
+                          padding: const EdgeInsets.all(20),
+                          margin: const EdgeInsets.fromLTRB(0, 0, 16, 0),
+                          child: const Icon(
+                            color: Color(0xFFadb1b8),
+                            size: 20,
+                            FeatherIcons.map,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 240,
+                      child: const Text(
+                        "welcome.feature4",
+                        style:
+                            TextStyle(color: Color(0xFFadb1b8), fontSize: 18),
+                      ).tr(),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 86),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      ref.read(appInitialisationProvider.notifier).set();
+                      ref
+                          .read(userLanguageProvider.notifier)
+                          .set(context.locale.languageCode);
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: WidgetStateProperty.all(
+                        const Color(0xFF1E90FF),
+                      ),
+                      padding: WidgetStateProperty.all(
+                        const EdgeInsets.symmetric(
+                          vertical: UiSpacing.spacingS,
+                        ),
+                      ),
+                      shape: WidgetStateProperty.all(
+                        RoundedRectangleBorder(borderRadius: UiSpacing.radiusM),
+                      ),
+                    ),
                     child: const Text(
-                      "welcome.feature1",
-                      style: TextStyle(color: Color(0xFFadb1b8), fontSize: 18),
+                      "actions.continue",
+                      style: TextStyle(fontSize: 18, color: Colors.white),
                     ).tr(),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 32),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    decoration: const BoxDecoration(
-                      color: Color.fromARGB(24, 173, 177, 184),
-                      borderRadius: BorderRadius.all(Radius.circular(500)),
-                    ),
-                    padding: const EdgeInsets.all(13),
-                    margin: const EdgeInsets.fromLTRB(0, 0, 16, 0),
-                    child: const Icon(
-                      color: Color(0xFFadb1b8),
-                      size: 24,
-                      FeatherIcons.award,
-                    ),
-                  ),
-                  SizedBox(
-                    width: 240,
-                    child: const Text(
-                      "welcome.feature2",
-                      style: TextStyle(color: Color(0xFFadb1b8), fontSize: 18),
-                    ).tr(),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 32),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    decoration: const BoxDecoration(
-                      color: Color.fromARGB(24, 173, 177, 184),
-                      borderRadius: BorderRadius.all(Radius.circular(500)),
-                    ),
-                    padding: const EdgeInsets.all(13),
-                    margin: const EdgeInsets.fromLTRB(0, 0, 16, 0),
-                    child: const Icon(
-                      color: Color(0xFFadb1b8),
-                      size: 24,
-                      FeatherIcons.mail,
-                    ),
-                  ),
-                  SizedBox(
-                    width: 240,
-                    child: const Text(
-                      "welcome.feature3",
-                      style: TextStyle(color: Color(0xFFadb1b8), fontSize: 18),
-                    ).tr(),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 32),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    decoration: const BoxDecoration(
-                      color: Color.fromARGB(24, 173, 177, 184),
-                      borderRadius: BorderRadius.all(Radius.circular(500)),
-                    ),
-                    padding: const EdgeInsets.all(13),
-                    margin: const EdgeInsets.fromLTRB(0, 0, 16, 0),
-                    child: const Icon(
-                      color: Color(0xFFadb1b8),
-                      size: 24,
-                      FeatherIcons.map,
-                    ),
-                  ),
-                  SizedBox(
-                    width: 240,
-                    child: const Text(
-                      "welcome.feature4",
-                      style: TextStyle(color: Color(0xFFadb1b8), fontSize: 18),
-                    ).tr(),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 86),
-              ElevatedButton(
-                style: ButtonStyle(
-                  fixedSize: MaterialStateProperty.all(
-                    const Size.fromWidth(310),
                   ),
                 ),
-                onPressed: () {
-                  ref.read(appInitialisationProvider.notifier).set();
-                  ref
-                      .read(userLanguageProvider.notifier)
-                      .set(context.locale.languageCode);
-                },
-                child: const Text("actions.continue").tr(),
-              ),
-            ],
+              ],
+            ),
           ),
         ],
       ),
